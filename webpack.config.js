01eaa53772999e5f16bb18c94d98754ce4987137
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-15 13:24:22 -0600

    Add: HTML Loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,33 @@
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
 
     output: {
-        path: path.resolve(__dirname, 'docs'),
+        clean: true, // Clean the output directory before emit
+        path: path.resolve(__dirname, 'docs'), // The output directory as an absolute path
       },
 
     module: {
-        rules: []
+        rules: [
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+            options: {
+              sources: false, // Disables attributes processing
+            },
+          }
+        ]
     },
 
     optimization: {},
 
-    plugins: [],
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'My Webpack App',
+        template: './src/index.html' // Load a custom template (lodash by default)
+      })
+    ],
   };
