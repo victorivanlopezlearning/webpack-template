66181348dc850b4bf844360f75a9932dcfc85152
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-16 14:09:15 -0600

    webpack.config.js: updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,12 @@
 const HtmlWebpackPlugin     = require('html-webpack-plugin');
 const MiniCssExtractPlugin  = require("mini-css-extract-plugin");
 const CopyPlugin            = require("copy-webpack-plugin");
-const path = require('path');
 
 module.exports = {
     mode: 'development',
 
     output: {
         clean: true, // Clean the output directory before emit
-        path: path.resolve(__dirname, 'docs'), // The output directory as an absolute path
         assetModuleFilename: 'assets/img/[hash][ext][query]',
       },
 
